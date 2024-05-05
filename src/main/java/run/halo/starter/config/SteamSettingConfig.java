3e5ce5e7c71993dd4a47363d6ff3b614c53719ca
weiyong <2623368495@qq.com>
2024-05-05 14:01:50 +0800

    优化代码

--- src/main/java/run/halo/starter/config/SteamSettingConfig.java
+++ src/main/java/run/halo/starter/config/SteamSettingConfig.java
@@ -1,14 +1,18 @@
 package run.halo.starter.config;
 
-import lombok.Data;
-import org.springframework.stereotype.Component;
 
-@Data
 public class SteamSettingConfig {
 
     public static final String CONFIG_MAP_NAME = "plugin-steam-config";
     public static final String GROUP = "steam";
 
-    private String token;
-    private String steamId;
+    /**
+     * Steam Web Api
+     * */
+    public static final String TOKEN =  "token";
+
+    /**
+     * Steam id
+     * */
+    public static final String STEAM_ID = "steamId";
 }
--- src/main/java/run/halo/starter/service/SteamService.java
+++ src/main/java/run/halo/starter/service/SteamService.java
@@ -7,19 +7,34 @@ import reactor.core.publisher.Mono;
 import run.halo.app.plugin.ReactiveSettingFetcher;
 import run.halo.starter.config.SteamSettingConfig;
 import run.halo.starter.entity.RecentlyPlayedGamesEntity;
+import run.halo.starter.utils.SteamUtils;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class SteamService {
     private final ReactiveSettingFetcher settingFetcher;
+
+    private static final WebClient webClient = WebClient.builder().baseUrl("http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/").build();
+
     public Mono<RecentlyPlayedGamesEntity> recentlyPlayedGames() {
 
         return settingFetcher.get(SteamSettingConfig.GROUP).flatMap(setting -> {
-            String key = setting.get("token").asText();
-            String id = setting.get("steamId").asText();
+            String key = setting.get(SteamSettingConfig.TOKEN).asText();
+            String id = setting.get(SteamSettingConfig.STEAM_ID).asText();
+
             String url = String.format("?key=%s&steamid=%s",key,id);
-            WebClient webClient = WebClient.builder().baseUrl("http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/").build();
-            return webClient.get().uri(url).retrieve().bodyToMono(RecentlyPlayedGamesEntity.class);
+            return webClient.get().uri(url).retrieve().bodyToMono(RecentlyPlayedGamesEntity.class).flatMap(
+                entity -> {
+                    List<RecentlyPlayedGamesEntity.GamesItem> updatedGames  =  entity.getResponse().getGames().stream().peek(
+                        item ->{
+                            //获取对应游戏的缩略图
+                            item.setImg_icon_url(SteamUtils.steamImage(item.getAppid(),item.getImg_icon_url()));
+                        }).toList();
+                    entity.getResponse().setGames(updatedGames);
+                    return Mono.just(entity);
+                }
+            );
         });
     }
 }
